2e78915ea429aa3ece98fbde612418586b950668
DinaHK2806 <dina.kozia1@gmail.com>
2026-04-23 15:42:22 +0300

    Added a cart and a checkout page, new order get saved to the db, added a profile page with the user orders information

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def serve_css():
 
 @app.get("/")
 def homepage():
-    with open("homepage.html") as f:
+    with open("homepage.html", encoding="utf-8") as f:
         return HTMLResponse(f.read())
 
 
@@ -149,3 +149,155 @@ async def post_review(product_id: int, request: Request):
     db.commit()
     db.close()
     return JSONResponse({"ok": True})
+
+
+@app.post("/api/cart/add")
+async def add_to_cart(request: Request):
+    data = await request.json()
+    username = request.cookies.get("username")
+
+    if not username:
+        return JSONResponse({"error: not logged in"}, status_code=401)
+    
+    product_id = data.get("product_id")
+    db = get_db()
+
+    existing = db.execute("SELECT * FROM cart WHERE username=? AND product_id=?", (username, product_id)).fetchone()
+
+    if existing:
+        db.execute("UPDATE cart SET quantity = quantity + 1 WHERE id=?", (existing["id"],))
+    
+    else:
+        db.execute("INSERT INTO cart (username, product_id, quantity) VALUES (?, ?, 1)", (username, product_id))
+
+    db.commit()
+    db.close()
+
+    return {"ok": True}
+
+@app.get("/api/cart/")
+def get_cart(request: Request):
+    username = request.cookies.get("username")
+
+    if not username:
+        return JSONResponse([], status_code=200)
+    
+    db = get_db()
+
+    items = db.execute("""
+        SELECT cart.id, products.name, products.price, products.image, cart.quantity
+        FROM cart
+        JOIN products ON cart.product_id = products.id
+        WHERE cart.username=?
+    """, (username,)).fetchall()
+    
+    db.close()
+    return JSONResponse([dict(item) for item in items])
+
+
+@app.delete("/api/cart/{item_id}")
+def remove_from_cart(item_id: int):
+    db = get_db()
+
+    row = db.execute(
+        "SELECT quantity FROM cart WHERE id=?", 
+        (item_id,)
+    ).fetchone()
+
+    if not row:
+        db.close()
+        return {"error": "item not found"}
+
+    quantity = row["quantity"] 
+
+    if quantity > 1:
+        new_quantity = quantity - 1
+        db.execute(
+            "UPDATE cart SET quantity = ? WHERE id=?",
+            (new_quantity, item_id)
+        )
+    else:
+        db.execute(
+            "DELETE FROM cart WHERE id=?",
+            (item_id,)
+        )
+        new_quantity = 0
+
+    db.commit()
+    db.close()
+
+    return {"quantity": new_quantity}  
+
+@app.get("/cart")
+def cart_page():
+    with open("cart.html", encoding="utf-8") as f:
+        return HTMLResponse(f.read())
+    
+
+@app.get("/checkout")
+def checkout_page():
+    with open("checkout.html", encoding="utf-8") as f:
+        return HTMLResponse(f.read())
+    
+@app.post("/api/checkout")
+def checkout(request: Request):
+    username = request.cookies.get("username")
+    db = get_db()
+
+    items = db.execute("""
+        SELECT product_id, quantity, products.price
+        FROM cart
+        JOIN products ON cart.product_id = products.id
+        WHERE cart.username=?
+    """, (username,)).fetchall()
+
+    total = sum(item["price"] * item["quantity"] for item in items)
+
+    cursor = db.execute(
+        "INSERT INTO orders (username, total) VALUES (?, ?)",
+        (username, total)
+    )
+    order_id = cursor.lastrowid
+
+    for item in items:
+        db.execute("""
+            INSERT INTO order_items (order_id, product_id, quantity)
+            VALUES (?, ?, ?)
+        """, (order_id, item["product_id"], item["quantity"]))
+
+    db.execute("DELETE FROM cart WHERE username=?", (username,))
+    db.commit()
+    db.close()
+
+    return {"message": "Order Completed"}
+
+@app.get("/profile")
+def profile_page():
+    with open("profile.html", encoding="utf-8") as f:
+        return HTMLResponse(f.read())
+    
+@app.get("/api/orders")
+def get_orders(request: Request):
+    username = request.cookies.get("username")
+
+    db = get_db()
+    orders= db.execute("SELECT * FROM orders WHERE username=?", (username,)).fetchall()
+    db.close()
+
+    return JSONResponse([dict(o) for o in orders])
+
+@app.get("/api/orders/{order_id}")
+def get_order_items(order_id: int):
+    db = get_db()
+
+    items = db.execute("""
+        SELECT products.name, products.image, products.price, order_items.quantity
+        FROM order_items
+        JOIN products ON order_items.product_id = products.id
+        WHERE order_items.order_id=?
+    """, (order_id,)).fetchall()
+
+    db.close()
+    return JSONResponse([dict(i) for i in items])
+
+
